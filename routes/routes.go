501f79171773f7654152bddbaa5e92106daa5b5d
Gabriel Mattos <gcostadev@gmail.com>
2024-12-13 17:03:40 -0300

    Roteador, recursos por ID e Docker

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 
 	"github.com/ga-pessoal/go-rest-api/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
