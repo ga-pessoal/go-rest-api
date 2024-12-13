ede903380922afa71f932b91ece0c075848227be
Gabriel Mattos <gcostadev@gmail.com>
2024-12-13 18:05:41 -0300

    Adicionamos um endpoint com método Post para criar uma nova personalidade e salvá-la no banco de dados; Adicionamos um endpoint com método Delete para deletar uma personalidade e removê-la do banco de dados; Adicionamos um endpoint com método Put para atualizar uma personalidade e alterá-la no banco de dados.

--- routes/routes.go
+++ routes/routes.go
@@ -10,8 +10,13 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriarUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletarUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.AlterarUmaPersonalidade).Methods("Put")
+
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
