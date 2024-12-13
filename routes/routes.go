c111c2a5d7c249a72c3568ed8753ae2c4186ce9e
Gabriel Mattos <gcostadev@gmail.com>
2024-12-13 18:52:22 -0300

    Indicamos o tipo do arquivo nas respostas da requisições incluindo o cabeçalho Content-Type;
    Criamos um middleware para evitar código duplicado no controller;
    
    Entendemos a importância da política de mesma origem e como compartilhar recursos de origens diferentes configurando o CORS;
    
    Configuramos o CORS e integramos a API Go com uma aplicação React.

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"github.com/ga-pessoal/go-rest-api/controllers"
+	"github.com/ga-pessoal/go-rest-api/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
@@ -18,5 +21,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletarUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.AlterarUmaPersonalidade).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
